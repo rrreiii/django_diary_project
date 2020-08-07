48179c03962221ca4bd6c5c0e610cd88acfbf58e
ReiHirasawa <reiman31@gmail.com>
2020-08-07 17:34:46 +0900

    first commit

---
+++ diary/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+app_name = 'diary'
+urlpatterns = [
+    path('', views.Index.as_view(), name='index'), #/diary
+    path('add/', views.add, name='add'), #/diary/add
+    path('update/<int:pk>', views.update,name='update'), #/diary/update/100
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('detail/<int:pk>', views.detail, name='detail')
+]
